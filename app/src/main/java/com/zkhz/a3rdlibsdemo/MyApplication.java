21376de18384eed4f149f0194e984019a87731c2
USER-20170614UA\Administrator <wangk0612@126.com>
2018-03-30 17:34:45 +0800

    no message

--- app/src/main/java/com/zkhz/a3rdlibsdemo/MyApplication.java
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/MyApplication.java
@@ -28,7 +28,13 @@ public class MyApplication extends Application {
 //        BlurKit.init(this);
 
         //友盟 初始化common库
-        UMConfigure.init(context,UMConfigure.DEVICE_TYPE_PHONE,Constants.UM_APP_KEY);
+        /**
+         * 初始化common库
+         * 参数1:上下文，不能为空
+         * 参数2:设备类型，UMConfigure.DEVICE_TYPE_PHONE为手机、UMConfigure.DEVICE_TYPE_BOX为盒子，默认为手机
+         * 参数3:Push推送业务的secret
+         */
+        UMConfigure.init(context,UMConfigure.DEVICE_TYPE_PHONE,null);
         UMConfigure.setLogEnabled(true);
     }
 
