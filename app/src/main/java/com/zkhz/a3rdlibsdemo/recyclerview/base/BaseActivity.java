0c2f27b328fc42ca4c1abcd5a8dd64316c4cc950
USER-20170614UA\Administrator <wangk0612@126.com>
2018-04-16 11:24:58 +0800

    1.baseActivity baseFragment封装

--- app/src/main/java/com/zkhz/a3rdlibsdemo/recyclerview/base/BaseActivity.java
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/recyclerview/base/BaseActivity.java
@@ -2,7 +2,12 @@ package com.zkhz.a3rdlibsdemo.recyclerview.base;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2018/4/13 0013.
@@ -13,8 +18,93 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        //在界面未初始化之前调用的初始化窗口
+        initWindows();
+
+        //如果界面初始化某些数据成功(传入的数据时正确的) 走初始化控件 数据;若初始化失败,
+        if (initArgs(getIntent().getExtras())){
+
+            getContentLayoutId();
+            initWidget();
+            initData();
+        }else {
+            finish();
+        }
+
     }
 
-    public abstract void getContentView();
 
+    /**
+     * 初始化一些窗口数据
+     */
+    protected void initWindows(){
+
+    }
+
+    //常规Activity间数据传递---Bundle
+    //初始化相关参数 如果参数正确,返回true
+    protected boolean initArgs(Bundle bundle) {
+        return true;
+    }
+
+    //protected---只要extends这个Activity就能复写这个方法
+    //返回当前界面的资源文件id
+    protected abstract int getContentLayoutId();
+
+
+    /**
+     * 初始化控件
+     */
+    protected void initWidget() {
+
+        ButterKnife.bind(this);
+
+
+    }
+
+
+    /**
+     * 初始化数据
+     */
+    protected void initData() {
+
+    }
+
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+
+        //当点击界面导航返回时,finish当前界面
+        finish();
+        return super.onSupportNavigateUp();
+    }
+
+
+    /**
+     * 假如activity有好多fragment,点击返回键,不想finish整个页面,想一层一层跳出
+     */
+    @Override
+    public void onBackPressed() {
+
+        //得到当前Activity下的所有Fragment
+        List<Fragment> fragments=getSupportFragmentManager().getFragments();
+        if (fragments!=null && fragments.size()>0){
+            for (Fragment fragment:fragments) {
+                //判断是否为我们能处理的Fragment类型
+                if (fragment instanceof BaseFragment){
+                    //判断是否拦截了返回键
+                    if (((BaseFragment) fragment).onBackPress()){
+                        //如果有,直接return
+                        return;
+                    }
+                }
+                
+            }
+        }
+        super.onBackPressed();
+        finish();
+    }
 }
+
---
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/recyclerview/base/BaseFragment.java
@@ -0,0 +1,107 @@
+package com.zkhz.a3rdlibsdemo.recyclerview.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by Administrator on 2018/4/16 0016.
+ */
+
+public abstract class BaseFragment extends Fragment {
+
+    //涉及到复用问题,将root保存起来
+    protected View mRoot;
+    protected Unbinder mRootUnBinder;
+
+
+    //当一个fragment被添加到Activity时,首先调用的是onAttach(),所以可在这个方法中进行初始化界面的一些操作
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        //初始化参数
+        initArgs(getArguments());
+
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        if (mRoot == null) {
+
+            int layoutId = getContentLayoutId();
+            //初始化当前的跟布局,但是不在(attach to root:false)创建时就添加到container里面
+            View root = inflater.inflate(layoutId, container, false);
+            initWidget(root);
+            mRoot = root;
+        }else {
+            if (mRoot.getParent()!=null){
+
+                //若fragment被回收,再重新初始化这个fragment时,有可能mRoot还没被回收,∴需要将当前root从其父控件中移除
+                ((ViewGroup) mRoot.getParent()).removeView(mRoot);
+            }
+
+        }
+
+
+        return mRoot;
+
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //当view创建完成之后初始化数据
+        initData();
+
+    }
+
+    //protected---只要extends这个Activity就能复写这个方法
+    //返回当前界面的资源文件id
+    protected abstract int getContentLayoutId();
+
+
+    //常规Activity间数据传递---Bundle
+    protected void initArgs(Bundle bundle) {}
+
+
+    /**
+     * 初始化控件
+     */
+    protected void initWidget(View root) {
+       mRootUnBinder= ButterKnife.bind(this,root);
+
+
+    }
+
+
+    /**
+     * 初始化数据
+     */
+    protected void initData() {
+
+    }
+
+
+    /**
+     * 返回按键触发时调用
+     * @return true---代表我已处理返回逻辑,Activity不用自己finish      false---代表我没处理逻辑,Activity自己走自己的逻辑.默认不拦截
+     */
+    public boolean onBackPress(){
+        return false;
+    }
+
+}
