a5d89e78b434b7b4f01363eb43220fbfb4318a5e
USER-20170614UA\Administrator <wangk0612@126.com>
2018-04-21 17:34:00 +0800

    no message

---
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/mvpdemo/gitmvpdemo/LoginInteractor.java
@@ -0,0 +1,8 @@
+package com.zkhz.a3rdlibsdemo.mvpdemo.gitmvpdemo;
+
+/**
+ * Interactor的作用实际上就是获取Model(从本地数据库，或者是服务器),转换成ViewModel，回调通知把ViewModel传递给Presenter。
+ */
+
+public interface LoginInteractor {
+}
--- app/src/main/java/com/zkhz/a3rdlibsdemo/mvpdemo/gitmvpdemo/LoginView.java
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/mvpdemo/gitmvpdemo/LoginView.java
@@ -1,8 +1,18 @@
 package com.zkhz.a3rdlibsdemo.mvpdemo.gitmvpdemo;
 
 /**
- * Created by Administrator on 2018/4/18 0018.
+ * view层描述
+ *
+ * 负责显示数据、提供友好界面跟用户交互就行
+ *
  */
 
 public interface LoginView {
+
+    void showProgress();
+    void hideProgress();
+    void setUserNameError();
+    void setPasswordError();
+    void navigateToHome();
+
 }
