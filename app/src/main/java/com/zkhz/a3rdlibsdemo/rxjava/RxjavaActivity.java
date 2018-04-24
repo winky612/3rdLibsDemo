26526c3600c26e8ba79fb84104aba1d5a39b76cc
USER-20170614UA\Administrator <wangk0612@126.com>
2018-04-24 17:33:27 +0800

    Rxjava(一)

---
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/rxjava/RxjavaActivity.java
@@ -0,0 +1,28 @@
+package com.zkhz.a3rdlibsdemo.rxjava;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zkhz.a3rdlibsdemo.R;
+
+/**
+ * Created by Administrator on 2018/4/24 0024.
+ */
+
+public class RxjavaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rxjava);
+
+        rxJava();
+    }
+
+    private void rxJava() {
+
+
+    }
+
+
+}
