028866d28f144312ce020f12904f9f1565d3b915
USER-20170614UA\Administrator <wangk0612@126.com>
2018-05-11 17:26:21 +0800

    no message

--- app/src/main/java/com/zkhz/a3rdlibsdemo/jpush/MyReceiver.java
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/jpush/MyReceiver.java
@@ -20,7 +20,7 @@ import cn.jpush.android.api.JPushInterface;
  * 自定义接收器
  * 
  * 如果不定义这个 Receiver，则：
- * 1) 默认用户会打开主界面
+ * 1) 默认用户会打开主界面 !!!!!!!!!!!
  * 2) 接收不到自定义消息
  */
 public class MyReceiver extends BroadcastReceiver {
