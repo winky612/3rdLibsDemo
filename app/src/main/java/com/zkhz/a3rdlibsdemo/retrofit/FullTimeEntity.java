e287d334dc4fe0a860966e47657f1a49a9aca0e7
USER-20170614UA\Administrator <wangk0612@126.com>
2018-07-09 16:25:51 +0800

    no message

---
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/retrofit/FullTimeEntity.java
@@ -0,0 +1,18 @@
+package com.zkhz.a3rdlibsdemo.retrofit;
+
+import java.util.List;
+
+public class FullTimeEntity {
+
+    //{"fullTimes":[2017-08-01,2018-08-03]}
+
+    private List<String> fullTimes;
+
+    public List<String> getFullTimes() {
+        return fullTimes;
+    }
+
+    public void setFullTimes(List<String> fullTimes) {
+        this.fullTimes = fullTimes;
+    }
+}
