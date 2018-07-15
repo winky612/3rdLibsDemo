8b7a9b8444edb32729049111c6721829360c5557
wk-PC\wk <wangk0612@126.com>
2018-07-15 20:43:12 +0800

    no message

--- app/src/main/java/com/zkhz/a3rdlibsdemo/mvpdemo/gitmvpdemo/login/LoginInteractor.java
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/mvpdemo/gitmvpdemo/login/LoginInteractor.java
@@ -3,7 +3,8 @@ package com.zkhz.a3rdlibsdemo.mvpdemo.gitmvpdemo.login;
 /**
  * Model
  *
- * Interactor的作用实际上就是获取Model(从本地数据库，或者是服务器),转换成ViewModel，回调通知把ViewModel传递给Presenter。
+ * Interactor的作用实际上就是获取Model(从本地数据库，或者是服务器),转换成ViewModel，
+ * 回调通知把ViewModel传递给Presenter。
  */
 
 public interface LoginInteractor {
