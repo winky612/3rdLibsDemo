92063170d385500f9b0d87e9350505c6c640557c
wk-PC\wk <wangk0612@126.com>
2018-07-15 21:59:16 +0800

    no message

--- app/src/main/java/com/zkhz/a3rdlibsdemo/coretopics/activity/fragment/LeftListFragment.java
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/coretopics/activity/fragment/LeftListFragment.java
@@ -16,9 +16,9 @@ import com.zkhz.a3rdlibsdemo.R;
 /**
  * 与 Activity 通信----创建对 Activity 的事件回调
  * <p>
- * 通过片段与 Activity 共享事件。执行此操作的一个好方法是，在片段内定义一个回调接口，并要求宿主 Activity 实现它。
+ * 通过fragment与 Activity 共享事件。执行此操作的一个好方法是，在fragment内定义一个回调接口，并要求宿主 Activity 实现它。
  * 当 Activity 通过该接口收到回调时，可以根据需要与布局中的其他片段共享这些信息。
- * 例如，如果一个新闻应用的 Activity 有两个片段 — 一个用于显示文章列表（片段 A），另一个用于显示文章（片段 B）— 那么片段 A 必须在列表项被选定后告知 Activity，以便它告知片段 B 显示该文章
+ * 例如，如果一个新闻应用的 Activity 有两个fragment — 一个用于显示文章列表（fragment A），另一个用于显示文章（fragment B）— 那么fragment A 必须在列表项被选定后告知 Activity，以便它告知fragment B 显示该文章
  */
 
 
@@ -43,7 +43,7 @@ public class LeftListFragment extends ListFragment {
         // 设置ListFragment默认的ListView
     }
 
-    //Activity 的每个连续状态如何决定片段可以收到的回调方法。 例如，当 Activity 收到其 onCreate() 回调时，Activity 中的片段只会收到 onActivityCreated() 回调。
+    //Activity 的每个连续状态如何 决定fragment可以收到的回调方法。 例如，当 Activity 收到其 onCreate() 回调时，Activity 中的fragment只会收到 onActivityCreated() 回调。
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -89,7 +89,7 @@ public class LeftListFragment extends ListFragment {
         super.onAttach(context);
 
         // 如果 Activity 未实现接口，则片段会引发 ClassCastException。
-        // 实现时，listener 成员会保留对 Activity 的 onTitleSelectedListener 实现的引用，以便片段 A 可以通过调用 OnArticleSelectedListener 接口定义的方法与 Activity 共享事件。
+        // 实现时，listener 成员会保留对 Activity 的 onTitleSelectedListener 实现的引用，以便fragment A 可以通过调用 OnArticleSelectedListener 接口定义的方法与 Activity 共享事件。
         // (联想----listener 通过 implements 牵住了实现它的那个activity,而listener是在fragment中定义的,也就等同于fragment通过listener 牵住了activity ---'风筝引线')
         try {
             listener = (onTitleSelectedListener) context;
