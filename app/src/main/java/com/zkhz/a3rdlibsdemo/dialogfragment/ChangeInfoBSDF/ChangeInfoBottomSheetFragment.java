f8959edd8b77d04edbd58bf9ef183bba5594ae88
wk-PC\wk <wangk0612@126.com>
2018-07-22 22:41:30 +0800

    activity向fragment传值

---
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/dialogfragment/ChangeInfoBSDF/ChangeInfoBottomSheetFragment.java
@@ -0,0 +1,94 @@
+package com.zkhz.a3rdlibsdemo.dialogfragment.ChangeInfoBSDF;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.zkhz.a3rdlibsdemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+/**
+ * Created by wk on 2018/7/22.
+ */
+
+public class ChangeInfoBottomSheetFragment extends BottomSheetDialogFragment {
+    private static final String KEY_TITLE = "KEY_TITLE";
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.edt_input)
+    EditText edtInput;
+    @BindView(R.id.btn_ok)
+    Button btnOk;
+    @BindView(R.id.btn_no)
+    Button btnNo;
+    Unbinder unbinder;
+    private String mTitle;
+    private String mContent;
+
+
+    private onChangeClickListener listener;
+
+    public static ChangeInfoBottomSheetFragment newInstance(String title) {
+        ChangeInfoBottomSheetFragment fragment = new ChangeInfoBottomSheetFragment();
+        Bundle args = new Bundle();
+        args.putString(KEY_TITLE, title);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            mTitle = args.getString(KEY_TITLE);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_bottom_change_info, null);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick({R.id.btn_ok, R.id.btn_no})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_ok:
+
+                mContent = edtInput.getText().toString();
+                listener = (onChangeClickListener) getActivity();
+                listener.onChangeClick(mContent);
+
+                break;
+            case R.id.btn_no:
+
+                dismiss();
+                break;
+        }
+    }
+
+
+    public interface onChangeClickListener {
+
+        void onChangeClick(String content);
+    }
+}
---
+++ app/src/main/java/com/zkhz/a3rdlibsdemo/dialogfragment/ChangeInfoBSDF/PersonInfoActivity.java
@@ -0,0 +1,58 @@
+package com.zkhz.a3rdlibsdemo.dialogfragment.ChangeInfoBSDF;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.zkhz.a3rdlibsdemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by wk on 2018/7/22.
+ */
+
+public class PersonInfoActivity extends AppCompatActivity {
+    @BindView(R.id.tv_name)
+    TextView tvName;
+    @BindView(R.id.tv_sex)
+    TextView tvSex;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_info);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.tv_name, R.id.tv_sex})
+    public void onViewClicked(View view) {
+
+        switch (view.getId()) {
+            case R.id.tv_name:
+
+
+                //nice
+                String title = tvName.getText().toString();
+                ChangeInfoBottomSheetFragment nameDialog = ChangeInfoBottomSheetFragment.newInstance(title);
+                nameDialog.show(getSupportFragmentManager(), "nameDialog");
+
+                break;
+            case R.id.tv_sex:
+
+                //bad
+                String titleSex = tvName.getText().toString();
+                Bundle bundle = new Bundle();
+                bundle.putString("sex",titleSex);
+                ChangeInfoBottomSheetFragment sexDialog = new ChangeInfoBottomSheetFragment();
+                sexDialog.setArguments(bundle);
+                sexDialog.show(getSupportFragmentManager(), "sexDialog");
+                break;
+        }
+    }
+
+}
